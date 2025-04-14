1bb6106e91eaff47a096f5a635bee557c56c6b3d
m4-x1 <marmolejomaxi@gmail.com>
2025-04-14 20:42:55 +0200

    Update Login.jsx
    Update Login Image

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -119,7 +119,7 @@ function Login() {
         className="auth-form"
       >
         <img
-          src="https://iberdesarrollos.es/wp-content/uploads/2025/02/Iberdes-Whatsapp.png"
+          src="https://iberdesarrollos.es/wp-content/uploads/2025/04/Iberdes-Whatsapp-1.png"
           alt="IberChat Logo"
           className="auth-logo"
         />
